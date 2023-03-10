a97ada804aabc9c9bbbe5cb31f874c7ef9743eca
amnlet <misteriodrey@gmail.com>
2023-03-10 14:15:04 -0300

    adcionando campos de login

--- jogoteca-data/app.py
+++ jogoteca-data/app.py
@@ -17,6 +17,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('lista.html', titulo="Jogos", jogos=lista)
+
+
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
@@ -34,5 +36,19 @@ def criar():
 def add_1():
     request.form['click']
     return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    tabela_aut = {'nome': "wesley", 'senha': '123'}
+    if tabela_aut['nome'] == request.form['usuario'] and tabela_aut['senha'] == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+
 
 app.run(host='localhost', port=8080, debug=True) 
